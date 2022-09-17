61c3591b3a2c64b6c75364ae3ce39a9fd1b64cbb
Ryuzaki13 <ryuzaki_dk_rm_guitar@mail.ru>
2022-09-17 23:13:25 +0500

    Файл sql с скриптом создания таблиц

---
+++ StudentManager/Sql/create.sql
@@ -0,0 +1,26 @@
+﻿create table if not exists specialty
+(
+    id            serial  not null primary key,
+    code          varchar not null,
+    caption       varchar not null,
+    qualification varchar not null,
+    constraint specialty_uq unique (qualification)
+);
+
+create table if not exists study_group
+(
+    number    varchar not null primary key,
+    course    integer not null default (1),
+    specialty integer not null,
+    constraint study_group_fk0 foreign key (specialty) references specialty (id),
+    constraint study_group_check check ( course > 0 and course < 6 )
+);
+
+create table if not exists student
+(
+    student_id  serial  not null primary key,
+    first_name  varchar not null,
+    last_name   varchar not null,
+    study_group varchar not null,
+    constraint student_fk0 foreign key (study_group) references study_group (number)
+);
