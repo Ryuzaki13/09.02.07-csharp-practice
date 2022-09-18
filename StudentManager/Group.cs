4dde023bba354656a73d99ad0d2a6804a70f051a
Ryuzaki13 <ryuzaki_dk_rm_guitar@mail.ru>
2022-09-18 23:45:27 +0500

    Добавлены стили

---
+++ StudentManager/Group.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using NpgsqlTypes;
+using StudentManager.AppData;
+
+namespace StudentManager {
+	public class Group {
+		private Specialty specialtyReference;
+
+		public Group() {
+			Course = 1;
+		}
+
+		public Group(string number, int course, int specialty) {
+			Number = number;
+			Course = course;
+			Specialty = specialty;
+		}
+
+		public Group(string number, int course, Specialty specialty) {
+			Number = number;
+			Course = course;
+			SpecialtyReference = specialty;
+		}
+
+		public string Number { get; set; }
+		public int Course { get; set; }
+		public int Specialty { get; set; }
+		public Specialty SpecialtyReference {
+			get { return specialtyReference; }
+			set {
+				specialtyReference = value;
+				if (specialtyReference != null) {
+					Specialty = specialtyReference.Id;
+				}
+			}
+		}
+
+		public bool Validate() {
+			if (Number == null) {
+				return false;
+			}
+
+			Number = Number.Trim();
+
+			return Number.Length > 0 && Course > 0 && Course < 6;
+		}
+
+		public bool Create() {
+			if (!Validate())
+				return false;
+
+			var command = Database.GetCommand("INSERT INTO study_group (number, course, specialty) VALUES (@a, @b, @c)");
+			command.Parameters.AddWithValue("@a", NpgsqlDbType.Varchar, Number);
+			command.Parameters.AddWithValue("@b", NpgsqlDbType.Integer, Course);
+			command.Parameters.AddWithValue("@c", NpgsqlDbType.Integer, Specialty);
+			var result = command.ExecuteNonQuery();
+
+			return result == 1;
+		}
+
+		public static void GetList(ObservableCollection<Group> list, ObservableCollection<Specialty> specialties) {
+			if (list == null)
+				return;
+
+			list.Clear();
+
+			var command = Database.GetCommand("SELECT number, course, specialty FROM study_group ORDER BY course, number");
+			var result = command.ExecuteReader();
+			if (result == null)
+				return;
+
+			if (result.HasRows) {
+				while (result.Read()) {
+
+					int id = result.GetInt32(2);
+
+					var specialty = specialties.Where(s => s.Id == id).FirstOrDefault();
+
+					list.Add(new Group(
+						result.GetString(0),
+						result.GetInt32(1),
+						specialty));
+				}
+			}
+
+			result.Close();
+		}
+	}
+}
--- StudentManager/MainWindow.xaml.cs
+++ StudentManager/MainWindow.xaml.cs
@@ -19,24 +19,42 @@ using StudentManager.AppData;
 namespace StudentManager {
 	public partial class MainWindow : Window {
 		public Specialty NewSpecialty { get; set; }
+		public Group NewGroup { get; set; }
 		public ObservableCollection<Specialty> Specialties { get; set; }
+		public ObservableCollection<Group> Groups { get; set; }
 
 		public MainWindow() {
 			InitializeComponent();
 
 			NewSpecialty = new Specialty();
+			NewGroup = new Group();
+
 			Specialties = new ObservableCollection<Specialty>();
+			Groups = new ObservableCollection<Group>();
 
 			Database.Connect(new Configuration("setting.json"));
 			LoadSpecialties();
-			// загрузка групп...
+			LoadGroups();
 			// загрузка студентов...
+
+			Binding specialtySourceBinding = new Binding();
+			specialtySourceBinding.Source = Specialties;
+
+			Binding groupSourceBinding = new Binding();
+			groupSourceBinding.Source = Groups;
+
+			SpecialtyList.SetBinding(ListBox.ItemsSourceProperty, specialtySourceBinding);
+			GroupList.SetBinding(ListBox.ItemsSourceProperty, groupSourceBinding);
 
 			DataContext = this;
 		}
 
 		private void LoadSpecialties() {
 			Specialty.GetList(Specialties);
+		}
+
+		private void LoadGroups() {
+			Group.GetList(Groups, Specialties);
 		}
 
 		private void CreateSpecialtyClick(object sender, RoutedEventArgs e) {
@@ -59,6 +77,17 @@ namespace StudentManager {
 		}
 
 		private void CreateGroupClick(object sender, RoutedEventArgs e) {
+			if (!NewGroup.Validate()) {
+				MessageBox.Show("Не все поля корректно заполненны");
+				return;
+			}
+			if (!NewGroup.Create())
+				return;
+
+			LoadGroups();
+
+			NewGroup = new Group();
+			GroupPanel.GetBindingExpression(DataContextProperty)?.UpdateTarget();
 		}
 		private void CreateStudentClick(object sender, RoutedEventArgs e) {
 		}
